1147dfbd86342733b55c00b2111ab2a3238232ae
Makendy PITON <makendy.piton@yahoo.fr>
2020-11-27 01:49:18 +0100

    Home page layout and customization

--- app/src/main/java/com/example/publicidentityprovider/MainActivity.kt
+++ app/src/main/java/com/example/publicidentityprovider/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.publicidentityprovider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSign_up.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Sign up user", Toast.LENGTH_SHORT).show()
+        }
     }
 }

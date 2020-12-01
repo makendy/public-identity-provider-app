9d34c3dfbac5606cb065c79ac5fd3dd60e766914
Makendy PITON <makendy.piton@yahoo.fr>
2020-12-02 00:28:04 +0100

    Regex for phone number

--- app/src/main/java/com/example/publicidentityprovider/src/SignUpActivity.kt
+++ app/src/main/java/com/example/publicidentityprovider/src/SignUpActivity.kt
@@ -2,13 +2,50 @@ package com.example.publicidentityprovider.src
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.publicidentityprovider.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val originIntent = intent
+
+
+        buttonSendSignUp.setOnClickListener {
+            if(verificationOfFields(textPhoneNumber.text.toString(), textEmailAddress.text.toString(),
+                            textLastName.text.toString(), textFirstName.text.toString(),
+                            textBirthDate.text.toString())) {
+                Log.d("SIGN UP OK", "Every fields is correct")
+            }
+            Log.d("SIGN UP", "phone number : " + textPhoneNumber.text +
+                                        " // email : " + textEmailAddress.text +
+                                        " // last name : " + textLastName.text +
+                                        " // first name : " + textFirstName.text +
+                                        " // birth date : " + textBirthDate.text)
+        }
+    }
+
+    private fun regexNumber (phoneNumber: String) : Boolean {
+        return Regex("^[0-9]*$").matches(phoneNumber)
+    }
+
+    private fun  verificationOfFields(
+            phoneNumber : String,
+            email : String,
+            lastName  : String,
+            firstName : String,
+            birthDate : String
+    ) : Boolean {
+
+        if(!regexNumber(phoneNumber)) {
+            Toast.makeText(this@SignUpActivity, "Phone number can contains only " +
+                    "digits", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 }

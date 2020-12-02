d30b69f7099bee2a0cfea737d104715842089a67
Makendy PITON <makendy.piton@yahoo.fr>
2020-12-02 22:46:45 +0100

    User class is created

---
+++ app/src/main/java/com/example/publicidentityprovider/details/User.kt
@@ -0,0 +1,7 @@
+package com.example.publicidentityprovider.details
+
+class User (val msisdn : String,
+            val email : String,
+            val lastName : String,
+            val firstName : String,
+            val birthDate : String)

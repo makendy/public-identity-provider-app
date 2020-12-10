a53ba48d04aa784ff62672f0668ad23041715bcd
Makendy PITON <makendy.piton@yahoo.fr>
2020-12-10 06:51:50 +0100

    Waiting app lists is done

--- app/src/main/java/com/example/publicidentityprovider/src/WaitingListActivity.kt
+++ app/src/main/java/com/example/publicidentityprovider/src/WaitingListActivity.kt
@@ -1,12 +1,19 @@
 package com.example.publicidentityprovider.src
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.publicidentityprovider.R
+import com.example.publicidentityprovider.adapter.WaitingAppListAdapter
 import com.example.publicidentityprovider.controller.RestApiService
 import com.example.publicidentityprovider.details.AppInfo
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_waiting_list.*
 
 class WaitingListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,40 @@ class WaitingListActivity : AppCompatActivity() {
     }
 
     private fun displayAppListInfo(listOfApp : List<AppInfo>) {
+        // display performance optimization when list widget size does not change
+        RecyclerListApp.setHasFixedSize(true)
+        // here we specify this is a standard vertical list
+        RecyclerListApp.layoutManager = LinearLayoutManager(
+            this@WaitingListActivity,
+            LinearLayoutManager.VERTICAL,
+            false)
+        //decoration
+        RecyclerListApp.addItemDecoration(DividerItemDecoration(this@WaitingListActivity, DividerItemDecoration.VERTICAL))
+        // attach an adapter and provide some data
 
+        val myAppClickListener = View.OnClickListener {
+            // we retrieve the row position from its tag
+            val position = it.tag as Int
+            val clickedItem = listOfApp[position]
+            // do stuff
+            Toast.makeText(
+                this@WaitingListActivity,
+                "Clicked " + clickedItem.appName,
+                Toast.LENGTH_SHORT)
+                .show()
+
+            // Create an explicit intent
+            //val explicitIntent = Intent(this@MainActivity,
+              //  GameDetailsActivity::class.java)
+            // Insert extra data in the intent
+            //explicitIntent.putExtra("GAME_ID", clickedItem.id.toString())
+            // Start the other activity by sending the intent
+            //startActivity(explicitIntent)
+        }
+        // attach an adapter and provide some data
+        val recyclerAdapter = WaitingAppListAdapter(listOfApp,this@WaitingListActivity,
+                                                        myAppClickListener)
+        RecyclerListApp.adapter = recyclerAdapter
     }
 
     private fun getWaitingAppList(userToken : String) {

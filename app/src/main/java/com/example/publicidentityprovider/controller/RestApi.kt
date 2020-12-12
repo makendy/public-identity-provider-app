6f436f6eb4812717a8fef674f4af7ed169e253a8
Makendy PITON <makendy.piton@yahoo.fr>
2020-12-12 13:27:49 +0100

    endpoint for post method for App authorization

--- app/src/main/java/com/example/publicidentityprovider/controller/RestApi.kt
+++ app/src/main/java/com/example/publicidentityprovider/controller/RestApi.kt
@@ -1,5 +1,6 @@
 package com.example.publicidentityprovider.controller
 
+import com.example.publicidentityprovider.details.AppAuthorization
 import com.example.publicidentityprovider.details.AppInfo
 import com.example.publicidentityprovider.details.UserInfo
 import com.example.publicidentityprovider.details.UserPostResponse
@@ -10,6 +11,10 @@ interface RestApi {
     @Headers("Content-Type: application/json")
     @POST("/users")
     fun addUser(@Body userData: UserInfo): Call<UserPostResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("/auth/authorize")
+    fun postAuthorize(@Body appAuthorization: AppAuthorization): Call<AppAuthorization>
 
     @Headers("Content-Type: application/json")
     @GET("/users/info")
--- app/src/main/java/com/example/publicidentityprovider/details/AppInfo.kt
+++ app/src/main/java/com/example/publicidentityprovider/details/AppInfo.kt
@@ -2,7 +2,7 @@ package com.example.publicidentityprovider.details
 
 import com.google.gson.annotations.SerializedName
 
-//---> User model for POST method
+//---> App model for POST method
 data class AppInfo (@SerializedName("appName") val appName : String,
                      @SerializedName("date") val date : String,
                      @SerializedName("scopes") val scopes : List<String>,

685da78f9b29fcadd92dedef793746f991a61d30
Makendy PITON <makendy.piton@yahoo.fr>
2020-12-12 13:49:14 +0100

    App authorization activity

---
+++ app/src/main/java/com/example/publicidentityprovider/src/AppAuthorizationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.publicidentityprovider.src
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.publicidentityprovider.R
+
+class AppAuthorizationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_authorization)
+    }
+}
